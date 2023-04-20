705bc8b021995b586a01a0e7cfead077a7cab083
ZhidieWu <93614558+ZhidieWu@users.noreply.github.com>
2023-04-20 21:11:28 +0200

    new file in step3

---
+++ src/main/java/Step3_Delete_Element/DeleteBoundaryEvent.java
@@ -0,0 +1,11 @@
+package Step3_Delete_Element;
+
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+
+import java.util.List;
+
+public class DeleteBoundaryEvent {
+    public static void delete(BpmnModelInstance modelInstance, List<NoAssociationTask> tasksToDelete) {
+
+    }
+}
